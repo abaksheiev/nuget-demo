28bc62779fed08594abbe8a1562cdfbf69ea40c3
AzureAD\AntonBaksheiev <anton.baksheiev@flyrlabs.com>
2024-07-31 09:33:16 +0200

    Init

---
+++ NugetDemo.ConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using NugetDemo.PackageDemo;
+using System.Dynamic;
+
+namespace NugetDemo.ConsoleApp;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        dynamic person = new ExpandoObject();
+        person.Name = "John Doe";
+        person.Age = 30;
+        person.Email = "john.doe@example.com";
+        person.Address = new ExpandoObject();
+        person.Address.Street = "123 Main St";
+        person.Address.City = "Anytown";
+        person.Address.ZipCode = "12345";
+
+
+        Console.WriteLine((person as object).ToJson());
+    }
+}
---
+++ NugetDemo.PackageDemo/Utils.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json;
+
+namespace NugetDemo.PackageDemo;
+
+public static class Utils
+{
+    public static string ToJson(this object data) {
+        return JsonSerializer.Serialize(data);
+    }
+
+    public static T From<T>(this string data)
+    {
+        return JsonSerializer.Deserialize<T>(data);
+    }
+}
